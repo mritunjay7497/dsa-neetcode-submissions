c7c9d156fd2c14d40325b1957dcdf279a04b3e14
mritunjay kumar mani <mkumarmani97@gmail.com>
2026-05-12 19:43:18 +0530

    Add: asteroid-collision - submission-8

---
+++ Data Structures & Algorithms/asteroid-collision/submission-8.java
@@ -0,0 +1,52 @@
+class Solution {
+    private boolean willCollide(Stack<Integer> st, int current) {
+        if (st.peek() > 0 && current < 0)
+            return true;
+        return false;
+    }
+
+    private int[] stToArr(Stack<Integer> st) {
+        return st.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 0; i < asteroids.length; i++) {
+            if (st.isEmpty()) {
+                st.push(asteroids[i]);
+            } else {
+                boolean collide = willCollide(st, asteroids[i]);
+                if (collide) {
+                    if (asteroids[i] + st.peek() == 0) {
+                        st.pop();
+                    } else {
+                        // keep fighting the asteroids in stack
+                        boolean alive = true;
+
+                        while (!st.isEmpty() && alive && willCollide(st, asteroids[i])) {
+                            if (Math.abs(asteroids[i]) > Math.abs(st.peek())) {
+                                st.pop();
+                            } else if (Math.abs(asteroids[i]) == Math.abs(st.peek())) {
+                                // both die -> ???
+                                alive = false;
+                                st.pop();
+                            } else {
+                                // current dies -> ???
+                                alive = false;
+                            }
+                        }
+                        if(alive){
+                            st.push(asteroids[i]);
+                        }
+                    }
+                    
+                } else {
+                    st.push(asteroids[i]);
+                }
+            }
+        }
+        int[] result = stToArr(st);
+        return result;
+    }
+}
