7293ea6363391c21750ef2773cb598321f3d1377
mritunjay kumar mani <mkumarmani97@gmail.com>
2026-06-10 21:50:00 +0530

    Add: longest-substring-without-duplicates - submission-11

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-11.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        
+        char[] strChar = s.toCharArray();
+        int length = s.length();
+        int maxLength = 0;
+
+
+// Bruteforce
+
+
+        // for(int i=0; i<length; i++){
+        //     String subStr = "";
+        //     Map<Character, Integer> map =  new HashMap<>();
+        //     for(int j=i; j<length; j++){
+
+        //         if(map.get(strChar[j]) != null){
+        //             break;
+        //         }
+        //         subStr+=strChar[j];
+        //         maxLength = maxLength > subStr.length() ? maxLength : subStr.length();
+        //         map.put(strChar[j],1);
+        //         // System.out.println(subStr);
+        //         // System.out.println(maxLength);
+
+        //     }
+        // }
+
+
+// Optimal 2-pointer
+
+        int lptr = 0;
+        int rptr = 0;
+
+        Map<Character,Integer> map = new HashMap<>();
+        while(rptr<length){
+
+            if(map.get(strChar[rptr]) != null && map.get(strChar[rptr]) >= lptr){
+                lptr = map.get(strChar[rptr]) + 1;
+            }
+            map.put(strChar[rptr], rptr);
+            int len = rptr-lptr+1;
+            maxLength = Math.max(len,maxLength);
+            rptr++;
+
+        }
+        return maxLength;
+    }
+}
