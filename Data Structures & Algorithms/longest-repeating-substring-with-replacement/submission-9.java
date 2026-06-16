01d2dd3fd61a8e83829ee704fea29aad65d8f71e
mritunjay kumar mani <mkumarmani97@gmail.com>
2026-06-16 07:45:07 +0530

    Add: longest-repeating-substring-with-replacement - submission-9

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-9.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int maxLength = 0;
+        int maxFrequency = 0;
+        int lptr=0;
+        int rptr=0;
+        int[] charCount = new int[26];
+        
+        // for(int i=0; i<s.length(); i++){
+        //     int[] charCount = new int[26];
+        //     for(int j=i; j<s.length(); j++){
+        //         // increament the char count of the current character
+        //         charCount[s.charAt(j) - 'A']++;
+
+        //         // calcluate the max-frequency for the characters encountered yet
+        //         maxFrequency = Math.max(maxFrequency, charCount[s.charAt(j) - 'A']);
+
+        //         // number of change which we can do is length - maxFrequency and it should be <=k
+        //         int changes = (j-i+1) - maxFrequency;
+        //         if(changes<=k){
+        //             maxLength = Math.max(maxLength,j-i+1);
+        //         }else{
+        //             break;
+        //         }
+        //     }
+        // }
+
+        while(rptr<s.length()){
+
+            // increament the char count of the current character
+            charCount[s.charAt(rptr) - 'A']++;
+
+            // calcluate the max-frequency for the characters encountered yet
+            maxFrequency = Math.max(maxFrequency, charCount[s.charAt(rptr) - 'A']);
+
+            // number of change which we can do is length - maxFrequency and it should be <=k
+            int changes = (rptr-lptr+1) - maxFrequency;
+
+            while(changes>k){
+                charCount[s.charAt(lptr) - 'A']--;
+                lptr++;
+                changes = (rptr-lptr+1) - maxFrequency;
+            }
+            if(changes<=k){
+                maxLength = Math.max(maxLength,rptr-lptr+1);
+            }
+            rptr++;
+        }
+        return maxLength;
+    }
+}
