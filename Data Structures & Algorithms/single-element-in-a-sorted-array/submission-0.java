a455d8e2962bf8817a944bf9623f0e580e13dda4
mritunjay kumar mani <mkumarmani97@gmail.com>
2026-07-12 14:18:10 +0530

    Add: single-element-in-a-sorted-array - submission-0

---
+++ Data Structures & Algorithms/single-element-in-a-sorted-array/submission-0.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int n = nums.length;
+
+        if(n==1) return nums[0];
+        if(nums[0] != nums[1]) return nums[0];
+        if(nums[n-2] != nums[n-1]) return nums[n-1];
+
+        int low = 1;
+        int high = n-2;
+        int mid = 0;
+
+        while(low<=high){
+            // if the single element is in the righ half, there will be (even,odd) idx on the left half
+            // we will eliminate the left half
+
+            // If single elment is in the left half, there will be (odd,even) idx on the right half.
+            // we will eliminate the right half
+
+            mid = low+(high-low)/2;
+
+            if(nums[mid-1] != nums[mid] && nums[mid+1] != nums[mid]){
+                return nums[mid];
+            }
+
+            if(mid%2 == 0){
+                if(nums[mid-1] == nums[mid]){
+                    // element is on the left side of mid
+                    // eliminate the right side of the mid
+                    high = mid - 1;
+                }else if(nums[mid+1] == nums[mid]){
+                    // element is on the right side of the mid
+                    // eliminate the left side
+                    low = mid;
+                }
+            }else{
+                if(nums[mid-1] == nums[mid]){
+                    // element is on the right side of mid
+                    // eliminate the left side of the mid
+                    low = mid + 1;
+                }else if(nums[mid+1] == nums[mid]){
+                    // element is on the left side of the mid
+                    // eliminate the right side
+                    high = mid - 1;
+                }
+            }
+            
+        }
+        return nums[mid];
+    }
+}
