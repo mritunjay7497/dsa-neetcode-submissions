a7348a0d33597b54bd0983d0bf3afabeae2020b1
mritunjay kumar mani <mkumarmani97@gmail.com>
2026-07-13 21:18:21 +0530

    Add: capacity-to-ship-packages-within-d-days - submission-1

---
+++ Data Structures & Algorithms/capacity-to-ship-packages-within-d-days/submission-1.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+        int minWeightCapacity = maxWeight(weights);
+        int maxWeightCapacity = sumOfAllWeights(weights);
+
+        int low = minWeightCapacity;
+        int high = maxWeightCapacity;
+
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            int daysTaken = calculateDays(mid,weights);
+            if(daysTaken <= days){
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return low;
+    }
+
+    // max weight capacity via which ship can transfer all weights in 1 day
+    private int sumOfAllWeights(int[] weights){
+        return Arrays.stream(weights).sum();
+    }
+
+    // min weight capacity of ship so trhat it can transfer all the weights, including heaviest
+    private int maxWeight(int[] weights){
+        return Arrays.stream(weights).max().getAsInt();
+    }
+
+    // days required according to min weiht capacity
+    private int calculateDays(int lwc, int[] weights){
+        int days = 1;
+        int load = 0;
+
+        // if a weight up[on adding is bvelow or equal to LWC, ship it same day
+        // otherwise, incease day count by 1 and start shipping from that package from next day
+        // This method makes trhe total TC to O(n*logm) but SC is O(1)
+        for(int i=0; i<weights.length; i++){
+            load += weights[i];
+            if(load <= lwc){
+                continue;
+            }else{
+                load = weights[i];
+                days++;
+            }
+        }
+        return days;
+    }
+}
